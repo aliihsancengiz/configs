8edb27b23b62b415e06d18445a3c12eb14ff36f4
Ali ihsan Cengiz <ali.ihsan.cengiz@profitap.com>
2023-12-01 12:57:03 +0100

    Add installer for dependencies

---
+++ installer.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+apt update
+
+install_base(){
+	apt install build-essential pkg-config git cmake python3 gcc g++ openssl clang clang-format wget curl openssh-server valgrind net-tools \
+		wireshark jq
+	systemctl enable ssh --now
+}
+
+
+install_dev(){
+	add-apt-repository ppa:jonathonf/vim
+	apt install zsh vim tmux fonts-powerline bear
+
+	# install fzf
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+
+	# install cargo and alacritty
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	cargo install alacritty
+
+	# vim plugin
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	
+	# oh my zsh install
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	curl -sL install-node.vercel.app/lts | bash
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+
+	# nertfonts
+	mkdir -p ~/.config
+	git clone git@github.com:ryanoasis/nerd-fonts.git ~/.config
+
+
+	# interactive installer
+	~/.fzf/install
+	~/.config/nerd-fonts/install.sh Hack
+	~/.config/nerd-fonts/install.sh Hack-NF
+	~/.config/nerd-fonts/install.sh Ubuntu
+}
